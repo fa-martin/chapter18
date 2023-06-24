b835a76b068c3615b34f5e872a05379037195586
fa-martin <f.a.martin@hotmail.com>
2023-06-24 11:40:31 -0300

    test

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -11,7 +11,7 @@ const Auth = () => {
           </div>
           <div className={classes.control}>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input type='password' id='password' /> 
           </div>
           <button>Login</button>
         </form>
