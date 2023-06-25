a22031e59bf57e10f3cc34621ae700a34c9228cd
fa-martin <f.a.martin@hotmail.com>
2023-06-25 15:58:00 -0300

    add redux dispatch and handler

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch(); // a function
+  const currectcounter = useSelector((state) => {
+    return state.counter;
+  });
   const toggleCounterHandler = () => {};
+  const toggleIncrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const toggleDecrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{currectcounter}</div>
+      <div className={classes.button}>
+        <button onClick={toggleIncrementHandler}>Increment</button>
+        <button onClick={toggleDecrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
